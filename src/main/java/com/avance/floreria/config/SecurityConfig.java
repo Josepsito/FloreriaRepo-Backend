d5ca0612c98f19539c49ef29a6e053755638db6c
josepsito <joseito97.15.jc@gmail.com>
2025-11-23 20:41:49 -0500

    cors2

--- src/main/java/com/avance/floreria/config/SecurityConfig.java
+++ src/main/java/com/avance/floreria/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200","https://floreriarepo-frontend.onrender.com"));
+
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
